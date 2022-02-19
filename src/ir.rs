b3e6167b210e3d53585c805837a4db760698800f
Simon Fell <fellforce@gmail.com>
2022-02-18 21:49:04 -0800

    Add enums/bitfields to iracing library

--- src/ir.rs
+++ src/ir.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::ffi::c_void;
 use std::ffi::CStr;
 use std::os::raw::c_char;
@@ -13,6 +15,10 @@ const IRSDK_MAX_STRING: usize = 32;
 // descriptions can be longer than max_string!
 const IRSDK_MAX_DESC: usize = 64;
 
+// define markers for unlimited session lap and time
+pub const IRSDK_UNLIMITED_LAPS: i32 = 32767;
+pub const IRSDK_UNLIMITED_TIME: f64 = 604800.0;
+
 bitflags! {
     pub struct StatusField:i32 {
         const CONNECTED = 1;
@@ -20,22 +26,196 @@ bitflags! {
 }
 
 bitflags! {
-    pub struct VarType:i32 {
-        // 1 byte
-        const CHAR = 0;
-        const BOOL = 1;
-
-        // 4 bytes
-        const INT=2;
-        const BITFIELD=3;
-        const FLOAT=4;
-
-        // 8 bytes
-        const DOUBLE=5;
-
-        //index, don't use
-        const ETCOUNT=6;
+    pub struct EngineWarnings:i32 {
+        const WATER_TEMP_WARNING    = 0x01;
+        const FUEL_PRESSURE_WARNING = 0x02;
+        const OIL_PRESSURE_WARNING  = 0x04;
+        const ENGINE_STALLED        = 0x08;
+        const PIT_SPEED_LIMITER     = 0x10;
+        const REV_LIMITER_ACTIVE    = 0x20;
+        const OIL_TEMP_WARNING      = 0x40;
     }
+}
+
+bitflags! {
+    pub struct Flags:u32 {
+        // global flags
+        const CHECKERED = 0x00000001;
+        const WHITE     = 0x00000002;
+        const GREEN     = 0x00000004;
+        const YELLOW    = 0x00000008;
+        const RED       = 0x00000010;
+        const BLUE      = 0x00000020;
+        const DEBRIS    = 0x00000040;
+        const CROSSED   = 0x00000080;
+        const YELLOW_WAVING = 0x00000100;
+        const ONE_TO_GREEN  = 0x00000200;
+        const GREEN_HELD    = 0x00000400;
+        const LAPS_10_TO_GO = 0x00000800;
+        const LAPS_5_TO_GO  = 0x00001000;
+        const RANDOM_WAVING = 0x00002000;
+        const CAUTION       = 0x00004000;
+        const CAUTION_WAVING= 0x00008000;
+
+        // driver black flags
+        const BLACK         = 0x00010000;
+        const DISQUALIFY    = 0x00020000;
+        const SERVICABLE    = 0x00040000;   // aka can pit
+        const FURLED        = 0x00080000;
+        const REPAIR        = 0x00100000;
+
+        // start lights
+        const START_HIDDEN  = 0x10000000;
+        const START_READY   = 0x20000000;
+        const START_SET     = 0x40000000;
+        const START_GO      = 0x80000000;
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum TrackLocation {
+    NotInWorld = -1,
+    OffTrack,
+    InPitStall,
+    ApproachingPits,
+    OnTrack,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum TrackSurface {
+    SurfaceNotInWorld = -1,
+    UndefinedMaterial = 0,
+
+    Asphalt1Material,
+    Asphalt2Material,
+    Asphalt3Material,
+    Asphalt4Material,
+    Concrete1Material,
+    Concrete2Material,
+    RacingDirt1Material,
+    RacingDirt2Material,
+    Paint1Material,
+    Paint2Material,
+    Rumble1Material,
+    Rumble2Material,
+    Rumble3Material,
+    Rumble4Material,
+
+    Grass1Material,
+    Grass2Material,
+    Grass3Material,
+    Grass4Material,
+    Dirt1Material,
+    Dirt2Material,
+    Dirt3Material,
+    Dirt4Material,
+    SandMaterial,
+    Gravel1Material,
+    Gravel2Material,
+    GrasscreteMaterial,
+    AstroturfMaterial,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum SessionState {
+    Invalid,
+    GetInCar,
+    Warmup,
+    ParadeLaps,
+    Racing,
+    Checkered,
+    CoolDown,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum CarLeftRight {
+    LROff,
+    LRClear,        // no cars around us.
+    LRCarLeft,      // there is a car to our left.
+    LRCarRight,     // there is a car to our right.
+    LRCarLeftRight, // there are cars on each side.
+    LR2CarsLeft,    // there are two cars to our left.
+    LR2CarsRight,   // there are two cars to our right.
+}
+
+bitflags! {
+    pub struct CameraState:i32 {
+        const IS_SESSION_SCREEN     = 0x0001; // the camera tool can only be activated if viewing the session screen (out of car)
+        const IS_SCENIC_ACTIVE      = 0x0002; // the scenic camera is active (no focus car)
+
+        //these can be changed with a broadcast message
+        const CAM_TOOL_ACTIVE           = 0x0004;
+        const UI_HIDDEN                 = 0x0008;
+        const USE_AUTO_SHOT_SELECTION   = 0x0010;
+        const USE_TEMPORARY_EDITS       = 0x0020;
+        const USE_KEY_ACCELERATION      = 0x0040;
+        const USE_KEY_10X_ACCELERATION  = 0x0080;
+        const USE_MOUSE_AIM_MODE        = 0x0100;
+    }
+}
+
+bitflags! {
+    pub struct PitSvcFlags:i32 {
+        const LF_TIRE_CHANGE	= 0x0001;
+        const RF_TIRE_CHANGE	= 0x0002;
+        const LR_TIRE_CHANGE    = 0x0004;
+        const RR_TIRE_CHANGE	= 0x0008;
+
+        const FUEL_FILL			= 0x0010;
+        const WINDSHIELD_TEAROFF= 0x0020;
+        const FAST_REPAIR		= 0x0040;
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum PitSvcStatus {
+    // status
+    PitSvNone = 0,
+    PitSvInProgress,
+    PitSvComplete,
+
+    // errors
+    PitSvTooFarLeft = 100,
+    PitSvTooFarRight,
+    PitSvTooFarForward,
+    PitSvTooFarBack,
+    PitSvBadAngle,
+    PitSvCantFixThat,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum PaceMode {
+    PaceModeSingleFileStart = 0,
+    PaceModeDoubleFileStart,
+    PaceModeSingleFileRestart,
+    PaceModeDoubleFileRestart,
+    PaceModeNotPacing,
+}
+
+bitflags! {
+    pub struct PaceFlags:i32 {
+        const END_OF_LINE   = 0x01;
+        const FREE_PASS     = 0x02;
+        const WAVED_AROUND  = 0x04;
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum VarType {
+    // 1 byte
+    CHAR = 0,
+    BOOL = 1,
+
+    // 4 bytes
+    INT = 2,
+    BITFIELD = 3,
+    FLOAT = 4,
+
+    // 8 bytes
+    DOUBLE = 5,
+
+    //index, don't use
+    ETCOUNT = 6,
 }
 
 #[repr(C)]
@@ -252,6 +432,112 @@ impl Client {
         }
         return false;
     }
+    pub fn dump_vars(&self) {
+        match self.header {
+            None => {}
+            Some(h) => unsafe {
+                let vhbase =
+                    self.shared_mem.add((*h).var_header_offset as usize) as *const IrsdkVarHeader;
+                for i in 0..(*h).num_vars {
+                    let vh = vhbase.add(i as usize);
+                    let var = Var { hdr: *vh };
+                    if var.hdr.count > 1 {
+                        match var.var_type() {
+                            VarType::BOOL => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.bools(&var)
+                            ),
+                            VarType::CHAR => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.chars(&var)
+                            ),
+                            VarType::INT => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.ints(&var)
+                            ),
+                            VarType::BITFIELD => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.bitfields(&var)
+                            ),
+                            VarType::FLOAT => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.floats(&var)
+                            ),
+                            VarType::DOUBLE => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.doubles(&var)
+                            ),
+                            VarType::ETCOUNT => todo!(),
+                        }
+                    } else {
+                        match var.var_type() {
+                            VarType::BOOL => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.bool(&var)
+                            ),
+                            VarType::CHAR => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.char(&var)
+                            ),
+                            VarType::INT => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.int(&var)
+                            ),
+                            VarType::BITFIELD => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.bitfield::<i32>(&var)
+                            ),
+                            VarType::FLOAT => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.float(&var)
+                            ),
+                            VarType::DOUBLE => println!(
+                                "{:20}: {:?}: {}: {:?}",
+                                var.name(),
+                                var.var_type(),
+                                var.count(),
+                                self.double(&var)
+                            ),
+                            VarType::ETCOUNT => todo!(),
+                        }
+                    }
+                }
+            },
+        }
+    }
     pub fn find_var(&self, name: &str) -> Option<Var> {
         match self.header {
             None => None,
@@ -270,7 +556,7 @@ impl Client {
             }
         }
     }
-    fn value<T: Copy>(&self, var: &Var, fortype: VarType) -> Option<T> {
+    pub fn value<T: Copy>(&self, var: &Var, fortype: VarType) -> Option<T> {
         if var.hdr.var_type == fortype {
             unsafe {
                 let x = self.data.as_ptr().add(var.hdr.offset as usize);
@@ -290,7 +576,7 @@ impl Client {
     pub fn int(&self, var: &Var) -> Option<i32> {
         self.value(var, VarType::INT)
     }
-    pub fn bitfield(&self, var: &Var) -> Option<i32> {
+    pub fn bitfield<T: Copy>(&self, var: &Var) -> Option<T> {
         self.value(var, VarType::BITFIELD)
     }
     pub fn float(&self, var: &Var) -> Option<f32> {
@@ -326,6 +612,14 @@ impl Client {
     }
     pub fn doubles(&self, var: &Var) -> &[f64] {
         self.values(var, VarType::DOUBLE)
+    }
+    pub fn session_info_update(&self) -> i32 {
+        unsafe {
+            match self.header {
+                None => -1,
+                Some(h) => (*h).session_info_update,
+            }
+        }
     }
     pub fn session_info(&self) -> &str {
         match self.header {
